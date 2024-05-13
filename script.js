64df6ba1437e86f6e3eb88c950e935e477d72320
EstefaníaH <tef.hermida@gmail.com>
2024-05-13 16:55:54 -0300

    collapsables work

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded event fired!");
-    const collapsibles = document.querySelectorAll(".Collapsible");
-    console.log(collapsibles); // Check if collapsibles are selected
-    collapsibles.forEach(function(collapsible) {
-      const button = collapsible.querySelector(".CollapseButton");
-      const content = collapsible.querySelector(".CardGroup");
-      console.log(button, content); // Check if button and content are selected
-      button.addEventListener("click", function() {
-        content.classList.toggle("active");
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const collapsibles = document.querySelectorAll(".Collapsible");
+  collapsibles.forEach(function (collapsible) {
+    const button = collapsible.querySelector(".IconButton");
+    const content = collapsible.querySelector(".CardGroup");
+
+    content.classList.add("hidden");
+    button.classList.add("hidden");
+    toggleIcon(button);
+
+    button.addEventListener("click", function () {
+      content.classList.toggle("hidden");
+      button.classList.toggle("hidden");
+      toggleIcon(button);
     });
   });
+
+  function toggleIcon(button) {
+    const collapsedIcon = button.querySelector(".CollapsedIcon");
+    const expandedIcon = button.querySelector(".ExpandedIcon");
+
+    collapsedIcon.classList.toggle(
+      "hidden",
+      !button.classList.contains("hidden")
+    );
+    expandedIcon.classList.toggle(
+      "hidden",
+      button.classList.contains("hidden")
+    );
+  }
+});
